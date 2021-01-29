cdeb6fb7c4bfe3246c05ed6ffa246128be8f70dc
rgon <rgon@outlook.com>
2021-01-29 19:13:56 +0100

    Relative import fix

--- xml_data_binding/__init__.py
+++ xml_data_binding/__init__.py
@@ -1,4 +1,4 @@
-from generated_code import *
+from .generated_code import *
 
 __all__ = ["image_descriptor", "image_handles_descriptor",
            "image_properties", "images_listing", "imaging_capabilities"]
